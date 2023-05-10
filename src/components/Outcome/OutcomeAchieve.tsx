123e2abb4d2ef381a2e376ae7c4fe674f147e951
achiewe <mamukashviliachi706@gmail.com>
2023-05-10 19:03:10 +0400

    finish OutcomeAchieve component

--- src/components/Outcome/OutcomeAchieve.tsx
+++ src/components/Outcome/OutcomeAchieve.tsx
@@ -1,3 +1,5 @@
+import NewWindowSvg from "../../images/icon-new-window.svg";
+
 import IconPlay from "../../images/icon-play.svg";
 interface Props {
   changeInput: boolean;
@@ -32,11 +34,11 @@ const OutcomeAchieve = ({ changeInput, saveInfo }: Props): JSX.Element => {
           />
         </div>
       </div>
-      <div className="w-full flex flex-row gap-[19px] items-center justify-start">
+      <div className="w-full flex flex-row gap-[16px] items-center justify-start">
         <h3
           className={`${
             changeInput ? "text-[#2D2D2D]" : "text-[#FFFFFF]"
-          } text-[18px] leading-[23.04px] font-normal`}
+          } text-[18px] leading-[23.04px] font-bold`}
         >
           noun
         </h3>
@@ -95,6 +97,80 @@ const OutcomeAchieve = ({ changeInput, saveInfo }: Props): JSX.Element => {
             >
               {saveInfo[0]?.meanings[0]?.definitions[2]?.definition}
             </p>
+          </div>
+        </div>
+        <div className="flex flex-row justify-start items-center gap-[24px]">
+          <h3 className="font-normal text-[16px] leading-[19.36px] text-[#757575]">
+            Synonyms
+          </h3>
+          <p className="text-[#A445ED] text-[16px] leading-[19.36px] font-bold">
+            electronic keyboard
+          </p>
+        </div>
+      </div>
+      <div className="w-full flex flex-row gap-[21px] items-center justify-start">
+        <h3
+          className={`${
+            changeInput ? "text-[#2D2D2D]" : "text-[#FFFFFF]"
+          } text-[18px] leading-[21.78px] font-bold`}
+        >
+          verb
+        </h3>
+        <hr
+          className={`w-full h-[1px]  ${
+            changeInput
+              ? "bg-[#E9E9E9], opacity-[1]"
+              : "bg-[#3A3A3A], opacity-[0.25]"
+          }`}
+        />
+      </div>
+      <div className="flex flex-col gap-[17px] items-start justify-center">
+        <h3 className="text-[16px] leading-[20.48px] text-[#757575] font-normal">
+          Meaning
+        </h3>
+        <div className="flex flex-col gap-[13px] justify-start items-center">
+          <div className="flex flex-row gap-[20px] justify-start items-start">
+            <div
+              className={
+                saveInfo[0]?.meanings[0]?.definitions[2]?.definition
+                  ? "bg-[#8F19E8] w-[5px] h-[5px] border-none rounded-full mt-[8px]"
+                  : "hidden"
+              }
+            />
+            <p
+              className={`text-[15px] ${
+                changeInput ? "text-[#2D2D2D]" : "text-[#FFFFFF]"
+              } leading-[24px] font-normal`}
+            >
+              {saveInfo[0]?.meanings[0]?.definitions[2]?.definition}
+            </p>
+          </div>
+          <p className="font-normal text-[15px] text-[#757575] leading-[24px]">
+            “Keyboarding is the part of this job I hate the most.”
+          </p>
+        </div>
+      </div>
+      <div className="flex flex-col gap-[24px] w-full justify-start items-center">
+        <hr
+          className={`w-full h-[1px]  ${
+            changeInput
+              ? "bg-[#E9E9E9], opacity-[1]"
+              : "bg-[#3A3A3A], opacity-[0.25]"
+          }`}
+        />
+        <div className="flex flex-col w-full justify-center items-start gap-[8px] ">
+          <h3 className="font-normal underline text-[14px] leading-[16.94px] text-[#757575]">
+            Source
+          </h3>
+          <div className="flex flex-row gap-[15px] w-full ">
+            <p
+              className={`text-[14px] underline leading-[16.94px] ${
+                changeInput ? "text-[#2D2D2D]" : "text-[#FFFFFF]"
+              } font-normal`}
+            >
+              https://en.wiktionary.org/wiki/keyboard
+            </p>
+            <img src={NewWindowSvg} alt="new window" />
           </div>
         </div>
       </div>
